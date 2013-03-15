0c9b1e49aa00922f84b5bb51fe511420a98c58b7
Shivanshu Singh <shivanshukumar@gmail.com>
2013-03-15 15:33:46 -0400

    the old mirror site deletion mechanism updated and improved.

--- PluginAnalyser/src/com/shivanshusingh/PluginAnalyser/Staging/DownloadAndStagingRunner.java
+++ PluginAnalyser/src/com/shivanshusingh/PluginAnalyser/Staging/DownloadAndStagingRunner.java
@@ -25,7 +25,7 @@ public class DownloadAndStagingRunner {
 		//  experimenting with the eclipse update manager from command line to get features
 		//  http://wiki.eclipse.org/Equinox_p2_Repository_Mirroring
 		String eclipseHome="/Users/singhsk/Developer/eclipse_sandbox/";//with the trailing  slash(/).
-		String eclipseApp="Eclipse.app/Contents/MacOS/eclipse";
+		String eclipseApp="Eclipse";
 		String equinoxAppName="org.eclipse.equinox.p2.artifact.repository.mirrorApplication";
 		String updateSiteURL=
 				//"http://download.eclipse.org/technology/dltk/updates/"
@@ -35,34 +35,23 @@ public class DownloadAndStagingRunner {
 		String destinationDirectory="/Users/singhsk/Developer/eclipse_plugins/testmirror_googleandroid1";//this is there the mirrired site would be available.
 		boolean writeModeClean=true, verbose=true, raw=true;
 		
-		String removeCommand=" rm -r ";boolean eraseOld=true;// this way old   mirror site can be complete removed first.
+		boolean eraseOld=true;// this way old   mirror site can be complete removed first.
 		
-		boolean previousCommandExecutedWithSuccess=true;
 		try {
-		    // Execute command
-			if(eraseOld)
-			{
-						File f = new File(destinationDirectory);
-						if(f.exists())
-						{
-							String command=removeCommand+" "+destinationDirectory+"/";
-							System.out.println("EXECUTING:"+command);
-							Process child = Runtime.getRuntime().exec(command);
-							try {
-								child.waitFor();
-							} catch (InterruptedException e) {
-								e.printStackTrace();
-							}
-							if(0!=child.exitValue())
-								previousCommandExecutedWithSuccess=false;
-							child.destroy();
-						}
+			if (eraseOld) {
+				File f = new File(destinationDirectory);
+				if (f.exists()) {
+					System.out.println("DELETING: " + f.getPath());
+
+					if (!deleteFolder(f)) {
+						System.err
+								.println("Error deleting old directory, not continuing forward");
+						return;
+					}
+
+				}
 			}
-			if(!previousCommandExecutedWithSuccess)
-			{
-				System.err.println("Error deleting old directory, not continuing forward");
-				return;
-			}
+			
 		    String command =eclipseHome + eclipseApp	+
 		    		(writeModeClean? " -writeMode clean" : "" )	+
 		    		(verbose?" -verbose":"")		+
@@ -72,21 +61,18 @@ public class DownloadAndStagingRunner {
 		    		" -source "+ updateSiteURL		+  
 		    		" -destination "  +"file:"+destinationDirectory;
 		    
-		    System.out.println("EXECUTING:"+command );
+		    System.out.println("EXECUTING: "+command );
 		    Process child = Runtime.getRuntime().exec(command);
 		    
 		    BufferedReader br= new  BufferedReader(new InputStreamReader(child.getInputStream()));
 		    int inread;
 		    while ((inread = br.read()) != -1)
-		    {
 		    	System.out.print((char)inread);
-		    }
+		    br.close();
 		    br= new  BufferedReader(new InputStreamReader(child.getErrorStream()));
 		    while ((inread = br.read()) != -1)
-		    {
 		    	System.out.print((char)inread);
-		    }
-		    
+		    br.close();
 		    System.out.println("exitvalue="+child.exitValue());
 		    
 		    try {
@@ -118,5 +104,27 @@ public class DownloadAndStagingRunner {
 		
 	
 	}
-
+	private static boolean deleteFolder(File folder) {
+		boolean success=true;
+		try{
+	    File[] files = folder.listFiles();
+	    if(files!=null) { //some JVMs return null for empty dirs
+	        for(File f: files) {
+	        	
+	            if(f.isDirectory()) {
+	              success=  deleteFolder(f);
+	            } else {
+	               success= f.delete();
+	            }
+	        }
+	    }
+	   success= folder.delete();
+		}
+		catch(Exception e)
+		{
+			success=false;
+		}
+		
+		return success;
+	}
 }
