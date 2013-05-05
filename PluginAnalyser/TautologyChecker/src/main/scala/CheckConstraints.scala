995b81b6527edb848a6626e7e70d6caa04c78fa8
Shivanshu Singh <shivanshukumar@gmail.com>
2013-05-05 01:11:32 -0400

    setting the max allowed constraint length (beyond which the constraint will be ignored from checking) to    2,500,000
    and also other  stats gathering

--- PluginAnalyser/TautologyChecker/src/main/scala/CheckConstraints.scala
+++ PluginAnalyser/TautologyChecker/src/main/scala/CheckConstraints.scala
@@ -2,6 +2,8 @@ import de.fosd.typechef.featureexpr.{ FeatureExprParser, FeatureExprFactory }
 
 object CheckConstraints extends App {
 
+  val allowedConstrLen=2500000;
+   
   FeatureExprFactory.setDefault(FeatureExprFactory.bdd)
 
   val rawfmFile = "fodder/dependencies.fm"
@@ -18,17 +20,66 @@ object CheckConstraints extends App {
   // val fm = FeatureExprFactory.dflt.featureModelFactory.create(parser.parseFile(fmFile))
   val fm = FeatureExprFactory.dflt.featureModelFactory.createFromDimacsFile(fmFile)
 
-  for (constraint <- constraints if !constraint.trim.isEmpty) {
+  println("==Max Constraint Length allowed, set to "+allowedConstrLen);
 
+  var maxConstrLenDetected=0
+  var minConstrLenDetected=allowedConstrLen
+  var constrCount=0
+  var avgConstrLen=0
+  var ignoredConstrCount=0
+  var notTautologyCount=0
+  var constrLens  = List[Int]()
+  for (constraint <- constraints if !constraint.trim.isEmpty) {
+    constrCount+=1
     println("checking " + constraint)
 
     val c = constraint.split("//").head
+    val constrLen=c.length()
+    constrLens= constrLen :: constrLens
+    avgConstrLen+=constrLen
 
+    if(constrLen>maxConstrLenDetected)
+      maxConstrLenDetected=constrLen;
+    if(minConstrLenDetected>constrLen)
+      minConstrLenDetected=constrLen;
+    if(constrLen<=allowedConstrLen)
+    {
     val expr = parser.parse(c)
 
     if (!expr.isTautology(fm))
       println("  not a tautology!")
+      notTautologyCount+=1
+    }
+    else
+    {
+      println("  expr ignored for being too long , >"+allowedConstrLen)
+      ignoredConstrCount+=1
+    }
 
   }
+  
+  avgConstrLen/=constrCount;
+  constrLens=constrLens.sortWith(_<_);
+
+  var lenMap:Map[Int,Int]=Map()
+  for(entry <- constrLens)
+  {
+    var currentSum=1;
+    if(lenMap.contains(entry))
+      currentSum+=lenMap(entry)
+    lenMap+=(entry ->  currentSum )
+  }
+
+  println("==Constraints ignored = "+ignoredConstrCount)
+  println("==Constraints ! Tautology = "+notTautologyCount)
+  println("=========")
+  println("==Min constraint length  detected = "+minConstrLenDetected)
+  println("==Max constraint length  detected = "+maxConstrLenDetected)
+  println("==Min constraint length  detected = "+minConstrLenDetected)
+  println("==Avg constraint length  detected = "+avgConstrLen)
+  println("==Med constraint length  detected = "+constrLens((constrLens.length/2)))
+  println(constrLens)
+  println("=========")
+  println(lenMap)
 
 }
