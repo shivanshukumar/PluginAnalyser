7d8d87478e4655b97eddbcb6056460c831f71fea
Shivanshu Singh <shivanshukumar@gmail.com>
2013-05-08 18:59:06 -0400

    correcting the go.sh script for running  the TautologyChecker

--- PluginAnalyser/TautologyChecker/go.sh
+++ PluginAnalyser/TautologyChecker/go.sh
@@ -2,5 +2,5 @@
 cp ../_OUTPUT___WorkingEclipseSite_features_and_plugins__/_OUTPUT/_DEPENDENCY_SET/dependencies.fm fodder/dependencies.fm
 cp ../_OUTPUT___WorkingEclipseSite_features_and_plugins__/_OUTPUT/_DEPENDENCY_SET/constraints.fm fodder/constraints.fm
 sbt mkrun
-./run.sh > fodder/output/results.ttlg
+./run.sh  CheckConstraints > fodder/output/results.ttlg
 echo Done.
